3dfe2ef6b4d235e26844a26b39e891afbaa79645
fertlopus <marat.movlamov@icloud.com>
2024-10-28 09:16:19 +0100

    ops: added variables for the main configuration script for the infrastructure.

---
+++ terraform/modules/resource_group/variables.tf
@@ -0,0 +1,23 @@
+variable "resource_group_name" {
+  description = "Name of the Resource Group (RG) for Azure Subscription"
+  type        = string
+  default     = "test"
+}
+
+variable "location" {
+  description = "Azure region for the Resource Group (RG)"
+  type        = string
+  default     = "westeurope"
+}
+
+variable "tags" {
+  description = "Tags to apply to the resource group for cost centre verification and identification"
+  type        = map(string)
+  default     = {
+    department = "dev"
+    app        = "wearthe"
+    environment= "dev"
+    lead_dev   = "Test"
+    lead_email = "test@test.test"
+  }
+}
