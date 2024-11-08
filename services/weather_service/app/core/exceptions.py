44e9e030546cdd458f5c27467f719d0eb123309b
fertlopus <marat.movlamov@icloud.com>
2024-11-08 22:55:03 +0100

    feat: Exception handlers added for the weather service.

---
+++ services/weather_service/app/core/exceptions.py
@@ -0,0 +1,33 @@
+from typing import Optional
+from fastapi import HTTPException, status
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+class WeatherServiceException(HTTPException):
+    """Base exception"""
+
+    def __init__(self, detail: str, status_code: int = status.HTTP_500_INTERNAL_SERVER_ERROR, log: bool = False):
+        super().__init__(status_code=status_code, detail=detail)
+        if log:
+            logger.error(f"{self.__class__.__name__}: {detail}")
+
+
+class OpenWeatherAPIException(WeatherServiceException):
+    """Raised when OpenWeather API fails"""
+
+    def __init__(self, detail: str, status_code: int = status.HTTP_503_SERVICE_UNAVAILABLE, log: bool = False):
+        super().__init__(status_code=status_code, detail=detail)
+        if log:
+            logger.error(f"{self.__class__.__name__}: {detail}")
+
+
+class WeatherDataNotFoundException(WeatherServiceException):
+    """Raised when weather data is not found/corrupted"""
+
+    def __init__(self, location: str, latitude: Optional[float] = None, longitude: Optional[float] = None):
+        detail = f"Weather data not found for location: {location}"
+        if latitude and longitude:
+            detail += f" (lat: {latitude}, lon: {longitude}"
+        super().__init__(status_code=status.HTTP_404_NOT_FOUND, detail=detail)
